acb4177cb32618b7a93c21c123d4ff12c4f2f1a3
Demon Singur <demonsingur@gmail.com>
2019-04-17 16:29:09 +0800

    daisy: Import Fingerprint HIDL
    Co-authored-by: Vasishath Kaushal <vasishath@gmail.com>
    Change-Id: I131df23b4f6f15d821f5adfe6476ec7d1690a322

--- device.mk
+++ device.mk
@@ -262,6 +262,10 @@ PRODUCT_PACKAGES += \
     ethertypes \
     libebtc
 
+# Fingerprint feature
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm6150
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
