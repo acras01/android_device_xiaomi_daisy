c6c1694027359f3ed73cd3b979be84340552fc64
starlight5234 <ifist2834@gmail.com>
2020-08-19 01:51:16 +0530

    daisy: Build libperfmgr
    Remove Lineage Powerhal from manifest
    
    Signed-off-by: starlight5234 <ifist2834@gmail.com>

--- device.mk
+++ device.mk
@@ -388,8 +388,7 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power@1.3-service.daisy-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
