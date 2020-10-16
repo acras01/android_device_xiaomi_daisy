2edbce22840a7f53e658b0614eae28a11c0cc94f
acras01 <acras1@gmail.com>
2020-10-16 10:38:21 +0300

    Add android.hardware.audio.effect@2.0-service to device.mk

--- device.mk
+++ device.mk
@@ -147,6 +147,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.3-impl \
     audio.r_submix.default \
     audio.usb.default \
