2f99d29c4b20c2a51a4b5c022a0bb9b68d977972
acras01 <acras1@gmail.com>
2020-10-23 15:03:26 +0300

    Revert "Build legacy sensors service"
    This reverts commit 5c40792c1a940877540aafe3356a7d34d51153fc.

--- device.mk
+++ device.mk
@@ -503,7 +503,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service.legacy \
+    android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
