9c8c6ff0b1e4f9d30bb07bf93981473a3cd9e3b6
acras01 <acras1@gmail.com>
2020-12-05 20:54:15 +0200

    Build android.hardware.camera.provider@2.6

--- device.mk
+++ device.mk
@@ -212,6 +212,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
+    android.hardware.camera.provider@2.6 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.postproc@1.0 \
     Snap \
