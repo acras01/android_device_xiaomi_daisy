925286505688fd0ef69e75daa5bc8b1cc185b3f5
acras01 <acras1@gmail.com>
2020-12-08 14:20:06 +0200

    Drop qti-telephony-common

--- device.mk
+++ device.mk
@@ -493,7 +493,6 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libcnefeatureconfig \
     libxml2 \
-    qti-telephony-common \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
