a579c2ee0fd39c8efeaff8f7a1fb60e46cd960fb
acras01 <acras1@gmail.com>
2020-12-15 15:52:48 +0200

    Build more BT packages

--- device.mk
+++ device.mk
@@ -170,7 +170,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.bluetooth.a2dp@1.0-impl \
     android.hardware.bluetooth.a2dp@1.0-service \
-    vendor.qti.hardware.bluetooth_dun@1.0
+    vendor.qti.hardware.bluetooth_dun@1.0 \
+    libbluetooth_audio_session
 
 #Audio Specific device overlays
 DEVICE_PACKAGE_OVERLAYS += hardware/qcom-caf/msm8996/audio/configs/common/overlay
@@ -200,6 +201,7 @@ PRODUCT_PACKAGES += \
     libbtconfigstore \
     libbluetooth_qti \
     libbthost_if \
+    bt_configstore \
     bt_did.conf \
     bt_stack.conf
 
