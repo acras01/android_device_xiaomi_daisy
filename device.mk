3235bd781303ea7c7d7e7f035a5aaee10ee5cba6
acras01 <acras1@gmail.com>
2021-01-28 16:40:36 +0200

    Rework QCOM FM

--- device.mk
+++ device.mk
@@ -290,11 +290,6 @@ PRODUCT_PACKAGES += \
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
-    libqcomfm_jni \
-    qcom.fmradio \
-    qcom.fmradio.xml
-
-PRODUCT_BOOT_JARS += \
     qcom.fmradio
 
 # Fwk-detect
